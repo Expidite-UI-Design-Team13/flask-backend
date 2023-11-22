2248118f77f3a8fe4b640750038ae4b3a74ccd30
Chance Onyiorah <chanceonyiorah@gmail.com>
2023-11-22 02:33:14 -0500

    JWT authentication for login and signup

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from flask_cors import CORS
 import json
 import pymysql
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, unset_jwt_cookies, jwt_required, JWTManager
 
 app = Flask(__name__)
 CORS(app)
+
+app.config["JWT_SECRET_KEY"] = "please-remember-to-change-me"
+jwt = JWTManager(app)
+
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, o):
@@ -46,8 +52,50 @@ def get_users():
 
     return rsp
 
-# get specific user from username and password
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
+# create access token on login
 @app.route("/api/users/login", methods = ["GET", "POST"])
+def create_token():
+    cur = get_cur()
+
+    json_data = request.get_json()
+
+    username  = json_data["username"]
+    password = json_data["password"]
+
+    cur.execute("SELECT * FROM expidite.users where username=%s and password=%s;", (username, password))
+
+    result = cur.fetchone()
+
+    if result:
+        rsp = Response(json.dumps(result, indent=4), status=200, content_type="application.json")
+    else:
+        return {"msg": "Wrong email or password"}, 401
+    
+    access_token = create_access_token(identity = username)
+    response = {"access_token": access_token}
+
+    return response
+
+# get specific user from username and password
+@app.route('/api/users/user', methods = ["GET", "POST"])
+@jwt_required()
 def get_user():
     cur = get_cur()
 
@@ -63,12 +111,18 @@ def get_user():
     if result:
         rsp = Response(json.dumps(result, indent=4), status=200, content_type="application.json")
     else:
-        rsp = Response(json.dumps([]), status=200, content_type="text/plain")
-
+        return {"msg": "Wrong username or password"}, 401
+    
     return rsp
 
+@app.route("/api/users/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
 # add new user
-@app.route("/api/users/add", methods = ["POST"])
+@app.route("/api/users/signup", methods = ["POST"])
 def add_user():
     cur = get_cur()
 
@@ -86,11 +140,12 @@ def add_user():
     else:
         rsp = Response(json.dumps([]), status=200, content_type="text/plain")
 
-    return rsp
+    return create_token()
 
 # get all items from specified user
 # user is determined by user_id in api request route
 @app.route("/api/items/<user_id>", methods = ["GET"])
+@jwt_required()
 def get_items_by_user(user_id):
     cur = get_cur()
     cur.execute("SELECT DISTINCT * FROM expidite.items where user_id=%s;", user_id)
@@ -105,6 +160,7 @@ def get_items_by_user(user_id):
 
 # add item for specified user
 @app.route("/api/items/<user_id>/add", methods = ["POST"])
+@jwt_required()
 def add_item(user_id):
     cur = get_cur()
 
